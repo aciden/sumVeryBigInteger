7e231b6155b5380bd8229088161b4664fca63bd7
Denis Moskalchuk <master@septempus.ru>
2018-12-26 14:38:28 +0300

    Update index.php

--- index.php
+++ index.php
@@ -10,11 +10,10 @@ function sumVeryBigInteger(string $number1, string $number2) {
         $partNumber2 = mb_substr($number2, -$sumPart);
         $number1 = mb_substr($number1, 0, -$sumPart);
         $number2 = mb_substr($number2, 0, -$sumPart);
-        if (($sum = (int) $partNumber1 + (int) $partNumber2 + $remainder)) {
-            $remainder = (int) mb_substr($sum, 0, -$sumPart);
-            $sum = (string) mb_substr($sum, -$sumPart);
-            $sumNumber = (string) $sum . $sumNumber;
-        }
+        $sum = (int) $partNumber1 + (int) $partNumber2 + $remainder;
+        $remainder = (int) mb_substr($sum, 0, -$sumPart);
+        $sum = (string) mb_substr($sum, -$sumPart);
+        $sumNumber = (string) $sum . $sumNumber;
 
     } while ($number1 || $number2);
 
